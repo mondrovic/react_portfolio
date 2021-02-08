dcaf3e3364fd17b04eecb52bdbc4e6cdbd1f5c1c
unknown <mondrovic1@gmail.com>
2021-02-07 23:29:18 -0700

    fixed routing

--- src/App.js
+++ src/App.js
@@ -13,9 +13,13 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path={"/"} exact component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
+          <Route path={process.env.PUBLIC_URL + "/"} exact component={Home} />
+          <Route path={process.env.PUBLIC_URL + "/about"} component={About} />
+          <Route
+            path={process.env.PUBLIC_URL + "/contact"}
+            component={Contact}
+          />
+          <Route path="*" component={Home} />
         </Switch>
         <Footer />
       </Router>
