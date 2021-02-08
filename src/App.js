f901f03e1a217464b4758690abae59cfc25a62b6
unknown <mondrovic1@gmail.com>
2021-02-08 00:53:16 -0700

    attempt at fixing routing

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Contact from "./components/pages/Contact";
 function App() {
   return (
     <>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
